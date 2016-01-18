5edd3ed4d194e75b6cd64df2b29294770e303324
buruoyanyang <xu18716352164@gmail.com>
2016-01-18 21:48:49 +0800

    Initial Commit

---
+++ tuangou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tuangou
+//
+//  Created by xhf on 16/1/18.
+//  Copyright © 2016年 xhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
